22c0dbbf33e3f2791e3025cafb523a003f141304
Steve Carratt <Steve@cloudhouse.local>
2016-07-26 10:07:01 +0100

    refactored settings

--- app/components/actions-view/actions-view.js
+++ app/components/actions-view/actions-view.js
@@ -9,24 +9,24 @@ ko.components.register('actions-view', {
 			
 			if (value.CloudloaderScript.Actions){
 				_.forIn(value.CloudloaderScript.Actions, function(actions, type){
-					if (_.isArray(actions)){
-						_.each(actions, function (action) {
-							if (action.Results && !_.isArray(action.Results.Result)){
-								action.Results = [action.Results.Result]
-							}else if (action.Results) {
-								action.Results = action.Results.Result
-							}
-							allActions.push(_.merge({type: type, id: action['-Id'], userMessage: action['-UserMessage']}, action))
-						});
+					if (_.isArray(actions)){			
+						_.each(actions, function (action) {addAction(action, type)});
 					} else {
-						if (actions.Results && !_.isArray(actions.Results.Result)){
-								actions.Results = [actions.Results.Result]
-						} else if (actions.Results){
-							actions.Results = actions.Results.Result
-						}
-						allActions.push(_.merge({type: type, id: actions['-Id'], userMessage: actions['-UserMessage']}, actions))
+						addAction(actions, type);
 					}
 				});
+			}
+			
+			function addAction(action, type) {
+				if (action.Results && !_.isArray(action.Results.Result)){
+					action.Results = [action.Results.Result];
+				}else if (action.Results) {
+					action.Results = action.Results.Result;
+				}else if (!action.Results){
+					action.Results = [];
+				}
+				
+				allActions.push(_.merge({type: type, id: action['-Id'], userMessage: action['-UserMessage']}, action))
 			}
 			
 			self.actions(allActions);
@@ -35,40 +35,16 @@ ko.components.register('actions-view', {
     template: 	'<h1>Actions</h1>'
 +	'<ul class="list-unstyled" data-bind="foreach: actions, as:action">'
 +		'<li class="well">'
-+		'<div class="row">'
-+			'<div class="col-md-6">'
-+				'<div class="form-horizontal">'
-+					'<div class="form-group">'
-+						'<label class="control-label col-sm-2">Id: </label>'
-+						'<div class="col-sm-10">'
-+							'<p class="form-control-static" data-bind="text: id"></p>'
-+						'</div>'
-+					'</div>'
-+					'<div class="form-group">'
-+						'<label class="control-label col-sm-2">Type: </label>'
-+						'<div class="col-sm-10">'
-+							'<p class="form-control-static" data-bind="text: type"></p>'
-+						'</div>'
-+					'</div>'
-+					'<div class="form-group">'
-+						'<label class="control-label col-sm-2">User Message: </label>'
-+						'<div class="col-sm-10">'
-+							'<p class="form-control-static" data-bind="text: userMessage"></p>'
-+						'</div>'
++		'<h2><span data-bind="text: id"></span><span>   </span><small data-bind="text: type"></small><span>   </span><small data-bind="text: userMessage"></small></h2>'
++		'<h4>Results</h4>'
++			'<div class="form-horizontal" data-bind="foreach: Results">'
++				'<div class="form-group">'
++					'<label class="control-label col-sm-3" data-bind="text: $data[\'-ResultText\']"> </label>'
++					'<div class="col-sm-6">'
++						'<p class="form-control-static" data-bind="text: $data[\'-ResultAction\']"></p>'
 +					'</div>'
 +				'</div>'
 +			'</div>'
-+			'<div class="col-md-6">'
-+				'<div class="form-horizontal" data-bind="foreach: Results">'
-+					'<div class="form-group">'
-+						'<label class="control-label col-sm-6" data-bind="text: $data[\'-ResultText\']"> </label>'
-+						'<div class="col-sm-6">'
-+							'<p class="form-control-static" data-bind="text: $data[\'-ResultAction\']"></p>'
-+						'</div>'
-+					'</div>'
-+				'</div>'
-+			'</div>'
-+		'</div>'
 +		'</li>'
 +	'</ul>'
 });
--- app/components/settings-editor/settings-editor.js
+++ app/components/settings-editor/settings-editor.js
@@ -1,33 +1,34 @@
 ko.components.register('settings-editor', {
     viewModel: function(params) {
 		var self=this;
-
-		self.postponedUpdateAllowed = ko.observable();
-		self.postponedUpdateMaxTries = ko.observable();
-		self.postponedUpdateForceUpdateDate	= ko.observable();
-		self.postponedUpdateMessage= ko.observable();
+		self.settings = ko.observable(
+		{
+			PostponedUpdateAllowed: ko.observable(),
+			PostponedUpdateMaxTries: ko.observable(),
+			PostponedUpdateForceUpdateDate: ko.observable(),
+			PostponedUpdateMessage: ko.observable()
+		});
+		
 		self.togglePostponedUpdateAllowed = function(){
-			self.postponedUpdateAllowed(!self.postponedUpdateAllowed());
+			ko.unwrap(self.settings).PostponedUpdateAllowed(!ko.unwrap(self.settings).PostponedUpdateAllowed());
 		}
 		
 		params.script.subscribe(function(value){
-			self.postponedUpdateAllowed(value.CloudloaderScript.ScriptSetting.PostponedUpdateAllowed === 'true')
-			self.postponedUpdateMaxTries(value.CloudloaderScript.ScriptSetting.PostponedUpdateMaxTries)
-			self.postponedUpdateForceUpdateDate(value.CloudloaderScript.ScriptSetting.PostponedUpdateForceUpdateDate)
-			self.postponedUpdateMessage(value.CloudloaderScript.ScriptSetting.PostponedUpdateMessage)
+			self.settings(ko.mapping.fromJS(value.CloudloaderScript.ScriptSetting))
 		});
 			
-		
-        
+		function save(){
+			value.CloudloaderScript.ScriptSetting = ko.toJS(self.settings)
+		}
     },
     template: 
 	'<h1>Settings</h1>'
 +	'<form>'
-+	'<div class="form-group"><button type="button" class="btn btn-primary" aria-controls="postponedUpdateSettings" data-toggle="collapse" autocomplete="off" data-target="#postponedUpdateSettings" data-bind="click: togglePostponedUpdateAllowed, attr: {\'aria-expanded\': postponedUpdateAllowed}, text: postponedUpdateAllowed() ? \'Postponed Update Allowed\': \'Postponed Update Not Allowed\'"></button></div>'
++	'<div class="form-group"><button type="button" class="btn btn-primary" aria-controls="postponedUpdateSettings" data-toggle="collapse" autocomplete="off" data-target="#postponedUpdateSettings" data-bind="click: togglePostponedUpdateAllowed, attr: {\'aria-expanded\': settings().PostponedUpdateAllowed}, text: settings().PostponedUpdateAllowed() ? \'Postponed Update Allowed\': \'Postponed Update Not Allowed\'"></button></div>'
 +	'<div class="collapse" id="postponedUpdateSettings">'
-+	'<div class="form-group"><label for="postponedUpdateMaxTries">PostponedUpdateMaxTries:</label> <input id="postponedUpdateMaxTries" class="form-control" type="text" data-bind="value: postponedUpdateMaxTries" /></div> '
-+	'<div class="form-group"><label for="postponedUpdateForceUpdateDate">PostponedUpdateForceUpdateDate:</label> <input id="postponedUpdateForceUpdateDate" class="form-control" type="datetime-local" data-bind="value: postponedUpdateForceUpdateDate" /></div> '
-+	'<div class="form-group"><label for="postponedUpdateMessage">PostponedUpdateMessage:</label> <textarea  id="postponedUpdateMessage" class="form-control" rows="3" data-bind="value: postponedUpdateMessage" /></div> '
++	'<div class="form-group"><label for="postponedUpdateMaxTries">PostponedUpdateMaxTries:</label> <input id="postponedUpdateMaxTries" class="form-control" type="text" data-bind="value: settings().PostponedUpdateMaxTries" /></div> '
++	'<div class="form-group"><label for="postponedUpdateForceUpdateDate">PostponedUpdateForceUpdateDate:</label> <input id="postponedUpdateForceUpdateDate" class="form-control" type="datetime-local" data-bind="value: settings().PostponedUpdateForceUpdateDate" /></div> '
++	'<div class="form-group"><label for="postponedUpdateMessage">PostponedUpdateMessage:</label> <textarea  id="postponedUpdateMessage" class="form-control" rows="3" data-bind="value: settings().PostponedUpdateMessage" /></div> '
 +	'</div>'
 + 	'</form>'
 });
